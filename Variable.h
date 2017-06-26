7c90f1346673c2bf308797503e7e298492f781b8
ohadwolski <ohadwolski@gmail.com>
2017-06-26 21:35:35 +0300

    variable declerations

--- Variable.h
+++ Variable.h
@@ -21,11 +21,48 @@ public:
 	
 	// ToDo: complete class definition
 	
+	Variable(); //c-tor
+	virtual ~Variable(); //d-tor
+
 	virtual VarPtr Copy() const = 0;
 	
 	virtual VarPtr operator+(const Variable&) const = 0;
 	virtual VarPtr operator+(const Scalar&) const = 0;
 	virtual VarPtr operator+(const Matrix&) const = 0;
+
+	virtual VarPtr operator*(const Variable&) const = 0;
+	virtual VarPtr operator*(const Scalar&) const = 0;
+	virtual VarPtr operator*(const Matrix&) const = 0;
+
+	virtual VarPtr operator<(const Variable&) const = 0;
+	virtual VarPtr operator<(const Scalar&) const = 0;
+	virtual VarPtr operator<(const Matrix&) const = 0;
+
+	virtual VarPtr operator>(const Variable&) const = 0;
+	virtual VarPtr operator>(const Scalar&) const = 0;
+	virtual VarPtr operator>(const Matrix&) const = 0;
+
+	virtual VarPtr operator==(const Variable&) const = 0;
+	virtual VarPtr operator==(const Scalar&) const = 0;
+	virtual VarPtr operator==(const Matrix&) const = 0;
+
+	virtual VarPtr operator&&(const Variable&) const = 0;
+	virtual VarPtr operator&&(const Scalar&) const = 0;
+	virtual VarPtr operator&&(const Matrix&) const = 0;
+
+	virtual VarPtr operator||(const Variable&) const = 0;
+	virtual VarPtr operator||(const Scalar&) const = 0;
+	virtual VarPtr operator||(const Matrix&) const = 0;
+
+	virtual VarPtr NumElems() const = 0;
+	virtual VarPtr Size() const = 0;
+	virtual VarPtr Size(int dim) const = 0;
+	virtual VarPtr Conv(VarPtr rhs) const = 0; ////needed to declare one for scalar and one for matrix?
+	virtual VarPtr Transpose() const = 0;
+	/////// printing << ??
+
+	virtual int& operator[](int idx) const = 0;
+	virtual int& operator[](IdxVec v) const = 0;
 
 };
 
